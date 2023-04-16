5a6372bcc04a79e0d4373bd95b7e971502e9fc47
Sade Whitcomb <swhitcomb@ufl.edu>
2023-04-15 23:56:22 -0400

    Added http requests to backend for user info

--- angular-project-files/src/app/app.component.ts
+++ angular-project-files/src/app/app.component.ts
@@ -2,6 +2,8 @@ import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { UserService } from './services/user.service';
 import { DescriptionBoxComponent } from './description-box/description-box.component';
+import { ViewChild } from '@angular/core';
+import { UsernameComponent } from './username/username.component';
 
 @Component({
   selector: 'app-root',
@@ -10,12 +12,19 @@ import { DescriptionBoxComponent } from './description-box/description-box.compo
 })
 
 export class AppComponent implements OnInit {
+  userComponent!: UsernameComponent;
+  
   title = 'BORE';
   subTitle = 'Join a room and share files';
-
+  user = this.userService.getUsername();
   showRectangle = false;
 
+  
+  
   username!: string | null;
+
+  
+
   ngOnInit() {
     this.username = localStorage.getItem('username');
   }
--- angular-project-files/src/app/services/user.service.ts
+++ angular-project-files/src/app/services/user.service.ts
@@ -2,9 +2,11 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable, throwError } from 'rxjs';
 import { catchError, retry } from 'rxjs/operators';
+import { Component, OnInit } from '@angular/core';
+import { map } from 'rxjs/operators';
 
-interface UserObject {
-  username: string;
+interface Name {
+  Name: string;
 }
 
 @Injectable({
@@ -12,10 +14,14 @@ interface UserObject {
 })
 export class UserService {
   name!: string;
+  debugString!: Name;
+  tempString!: string;
+  tempString1!: string;
+  username!: string;
 
   tempName!: string;
   ip = location.host;
-  private apiUrl = 'http://' + this.ip + '3621';
+  private apiUrl = 'http://localhost:3621';
   //private user: UserObject = {name: this.name};
 
   constructor(private http: HttpClient) {}
@@ -24,10 +30,67 @@ export class UserService {
     this.name = name;
   }
 
+  ngOnInit() {
+    
+  }
+
+  updateUsername(): string {
+    return this.returnUsername();
+  }
+
+  returnUsername(): string {
+    const str = this.getUsername();
+    const word = this.extractWord(str);
+    return word;
+  }
+
+  extractWord(str: string): string {
+    const match = str.match(/"([^"]*)"/g);
+    if (match && match.length > 1) {
+      return match[1].replace(/"/g, '');
+    }
+    return '';
+  }
+  // getDebug() {
+  //    this.tempName = this.apiUrl + '/debug';
+  //    return this.http.get<Name>(this.tempName);
+  // }
+
+  // getUsername(): Observable<string> {
+  //   this.tempName = this.apiUrl + '/debug';
+  //   return this.http.get<Name>(this.tempName).pipe(
+  //     map((data: Name) => {
+  //       this.debugString = data;
+  //       console.log(this.debugString.Name); // should output "debug"
+  //       return this.debugString.Name;
+  //     })
+  //   );
+  // }
   getUsername(): string {
-    return this.name;
+    this.tempName = this.apiUrl + '/debug';
+    this.http.get(this.tempName).pipe(
+      map(data => JSON.stringify(data))
+    )
+    .subscribe(result => {
+      this.tempString = result;
+      console.log(this.tempString); // Prints the converted string to the console
+    });
+    return this.tempString;
   }
   
+  getUsername1(): string {
+    this.tempName = this.apiUrl + '/debug';
+    this.http.get<Name>(this.tempName).pipe(
+      map(data => (data))
+    )
+    .subscribe(result => {
+      this.debugString = result;
+      this.tempString1 = JSON.stringify(result.Name);
+      console.log(this.tempString1); // Prints the converted string to the console
+    });
+
+    return this.tempString1;
+  }
   
   postUsername(name: string) {
     this.tempName = this.apiUrl + '/setName';
